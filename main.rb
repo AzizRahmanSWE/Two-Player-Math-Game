7fde33ac0b21f1d060667ef7510f88a3428bde4d
Friies <bahram.parsa2019@gmail.com>
2023-08-13 15:43:59 -0400

    Allow for player names input

--- main.rb
+++ main.rb
@@ -3,9 +3,17 @@ require './01_player_class.rb'
 require './02_game_class.rb'
 require './03_math_question_class.rb'
 
-player1 = Player.new('Aziz 1')
-player2 = Player.new('Aziz 2')
+# Asks player names
+puts "Enter Player 1's Name"
+player1_name = gets.chomp
 
+puts "Enter Player 2's Name"
+player1_name = gets.chomp
 
+# Creates player objects with the provided name
+player1 = Player.new(player1_name)
+player2 = Player.new(player2_name)
+
+# Starts the game by creating player class with the two players
 game = Game.new(player1, player2)
 game.game_loop
